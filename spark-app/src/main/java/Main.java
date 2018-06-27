514b894c086a5bd9b3461aaf6465d23d60220124
a305468917 <305468917@qq.com>
2018-06-28 00:43:17 +0800

    delete spark

--- spark-app/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import static spark.Spark.*;
-
-public class Main {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-        //staticFiles.location("/public");
-    }
-}
